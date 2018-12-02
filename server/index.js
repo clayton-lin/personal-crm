ac80c7b1cbf841692db0712866390b402dbb6a4b
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-02 15:29:32 -0600

    Connect database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,25 @@
+require('dotenv').config({path: '../.env'});
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM cities', (err, rows, fields) => {
+  if (err) throw err;
+  console.log(rows[0]);
+})
+
+connection.end();
+
+
 const app = express();
 const port = 8000;
 
