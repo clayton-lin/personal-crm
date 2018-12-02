a508bb1c8f5ef61836b16860d6ed56aa8e6c1e30
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-01 20:30:22 -0600

    Add table 'people' to schema

---
+++ database/schema.sql
@@ -0,0 +1,24 @@
+-- ---
+-- HOW TO USE 
+-- PIPE THIS FILE INTO YOUR MYSQL DATABASE TO CLEAN THE DB AND TABLES
+--
+--
+
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+
+--  DROP DATABASE IF EXISTS crm;
+--  CREATE DATABASE crm;
+
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+USE crm;
+
