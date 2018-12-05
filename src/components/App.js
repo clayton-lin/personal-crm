6e19a4e381a5322ca87f67f2407424984ecc908d
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-04 22:21:06 -0600

    Add basic React files in src folder

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Personal-CRM App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
