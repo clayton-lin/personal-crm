cac7e623bfdbc051303bf4f6ea32f7cbbaec2d5a
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-06 21:51:21 -0600

    Move db connection to separate file

---
+++ database/index.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect();
+connection.query('SELECT * FROM crm_cities', (err, rows, fields) => {
+  if (err) throw err;
+  console.log(rows[0]);
+});
--- server/index.js
+++ server/index.js
@@ -5,32 +5,15 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const database = require('../database/index.js');
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
 
-connection.connect();
-connection.query('SELECT * FROM crm_cities', (err, rows, fields) => {
-  if (err) throw err;
-  console.log(rows[0]);
-});
 
 const app = express();
 app.use(bodyParser.json());
 const port = 8000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
-
-// app.get('*', (req, res) => {
-//   res.redirect('/');
-// });
-
-// app.get('/', (req, res) => res.send('../src/index.html'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
