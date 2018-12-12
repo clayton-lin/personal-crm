bfe3066c09da660442c6543c356e70d731a35a14
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-11 18:42:47 -0600

    Add function to retrieve all contacts for given user

--- database/index.js
+++ database/index.js
@@ -11,3 +11,24 @@ connection.query('SELECT * FROM crm_cities', (err, rows) => {
   if (err) throw err;
   console.log(rows[0]);
 });
+
+const retrieveAllContacts = (user, callback) => {
+  const { userId } = user;
+  const queryString = `
+    SELECT * FROM contacts WHERE user_id = ${userId}
+  `;
+  connection.query(queryString, (err, results) => {
+    if (err) {
+      console.log(`error: contact retrieval for user ${userID} failed`);
+      console.log(err);
+      callback(err, null);
+    } else {
+      console.log(`contacts for user ${userId} sucessfully retrieved`);
+      callback(null, results);
+    }
+  });
+}
+
+module.export = {
+  retrieveAllContacts
+}
