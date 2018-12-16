bd0c9bc6ae15c36c8dc50d5fb93d10cc38cf6d38
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-16 14:40:38 -0600

    Update insert promise chain to check if contact exists and add if not

--- server/addContactTest.js
+++ server/addContactTest.js
@@ -1,15 +1,17 @@
 const contacts = require('../database/dbFunctions/contacts');
 
 const user = {
-  userId: 0
+  userId: 0,
+  preferredName: 'Clayton',
+  familyName: 'Lin'
 }
 
 const contact = {
   givenName: 'Clayton',
-  preferredName: 'Clayton',
+  preferredName: 'Claytone',
   middleName: 'YT',
   familyName: 'Lin', 
-  maidenName: null,
+  maidenName: '',
   gender: 'male',
   photo: null,
   birthYear: 1989,
@@ -17,20 +19,32 @@ const contact = {
   birthDay: 10,
 }
 
-// promise chain
-// check if contact exists in database
-// if yes,
-  // return message that says contact already in db
-// if no
-  // add contact and return sucess message and contact id
-  // add phone number to db
-  // add email to db
+// verify whether contact exists in database for user
+// searches using preferred_name and family_name
+contacts.verifyContactExists(user, contact)
+// if contact exists, throw error
+// else add contact
+.then((data) => {
+  const { user, contact, rows } = data;
 
+  if (rows[0]) {
+    const found = rows[0];
+    throw `contact '${found.preferred_name}' '${found.family_name}' already exists for user ${user.userId}`;
+  }
 
-
-
-
-contacts.addContact(user, contact, (err, results) => {
-  console.log('COMPLETE');
-  console.log(results);
+  return contacts.addContact(user, contact);
 })
+// then add phone number to table referencing contact's id
+.then((data) => {
+  const { user, contact, rows } = data;
+  const contactId = rows.insertId;
+
+})
+// then add email to table referencing contact's id
+.then((data) => {
+
+})
+.catch((err) => {
+  console.log(err);
+});
+
