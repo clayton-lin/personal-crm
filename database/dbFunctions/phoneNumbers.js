ba92adeacb1a1fb715fec2393cb38d43f5e0f6bf
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-20 21:20:34 -1000

    Make minor corrections to functions

--- database/dbFunctions/phoneNumbers.js
+++ database/dbFunctions/phoneNumbers.js
@@ -4,7 +4,7 @@ const mysql = require('../index.js');
 const retrievePhoneNumbers = (contactId) => {
   return new Promise((resolve, reject) => {
     const queryString = `
-      SELECT * FROM phone_numbers WHERE contact_id = ${contactId};
+      SELECT * FROM phone_numbers WHERE contacts_id = ${contactId};
     `;
   
     mysql.connection.query(queryString, (err, rows, fields) => {
@@ -25,7 +25,7 @@ const verifyPhoneNumberExists = (contactId, phoneNumber) => {
   return new Promise((resolve, reject) => {
     const queryString = `
       SELECT * FROM phone_numbers WHERE 
-        contactId = ${contactId} AND digits = '${phoneNumber}'
+        contactId = ${contactId} AND digits = '${phoneNumber}';
     `;
   
     mysql.connection.query(queryString, (err, rows, fields) => {
@@ -47,7 +47,7 @@ const addPhoneNumber = (contactId, phoneNumber) => {
     // phone number is already formatted to XXX-XXX-XXXX
     const queryString = `
       INSERT INTO phone_numbers (contacts_id, digits) VALUES
-        (${contactId}, '${phoneNumber}')
+        (${contactId}, '${phoneNumber}');
     `;
   
     mysql.connection.query(queryString, (err, rows, fields) => {
@@ -69,8 +69,8 @@ const updatePhoneNumber = (contactId, phoneNumber) => {
     // phone number is already formatted to XXX-XXX-XXXX
     const queryString = `
       UPDATE phone_numbers
-        SET digits = '${phoneNumber}
-        WHERE id = ${contactId}
+        SET digits = '${phoneNumber}'
+        WHERE id = ${contactId};
     `;
   
     mysql.connection.query(queryString, (err, rows, fields) => {
