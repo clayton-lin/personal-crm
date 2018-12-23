69f829f009706f817e9b90ef9734d76704e25937
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-22 16:09:50 -1000

    Refactor test function to use async/await

--- server/addContactTest.js
+++ server/addContactTest.js
@@ -24,48 +24,75 @@ const contact1 = {
   email: 'philip.j.coulson@shield.com'
 };
 
+// // using promises
+// const addNewContact = (user, newContact) => {
+//   const { userId } = user;
+//   const { preferredName, familyName, phoneNumber, email } = newContact;
+//   let contactId;
+//   let phoneNumberId;
+//   let emailId;
 
-const addNewContact = (user, newContact) => {
-  const { userId } = user;
-  const { preferredName, familyName, phoneNumber, email } = newContact;
-  let contactId;
-  let phoneNumberId;
-  let emailId;
+//   return contacts.verifyContactExists(user, newContact)
+//     .then((data) => {
+//       if (data[0]) {
+//         const { preferred_name, family_name} = data[0];
+//         throw `Contact '${preferred_name} ${family_name}' already exists for user ${user.userId}`;
+//       }
 
-  return contacts.verifyContactExists(user, newContact)
-    .then((data) => {
-      if (data[0]) {
-        const { preferred_name, family_name} = data[0];
-        throw `Contact '${preferred_name} ${family_name}' already exists for user ${user.userId}`;
+//       return contacts.addContact(user, newContact);
+//     })
+//     .then((data) => {
+//       contactId = data.insertId;
+//       console.log(`Contact '${preferredName} ${familyName}' added for userId = ${userId}, contactId = ${contactId}`);
+
+//       return phoneNumbers.addPhoneNumber(contactId, phoneNumber);
+//     })
+//     .then((data) => {
+//       phoneNumberId = data.insertId;
+//       console.log(`Phone number '${phoneNumber}' added for contactId = ${contactId}, phoneNumberId = ${phoneNumberId}`);
+
+//       return emails.addEmail(contactId, email);
+//     })
+//     .then((data) => {
+//       emailId = data.insertId;
+//       console.log(`Email '${email}' added for contactId = ${contactId}, emailId = ${emailId}`);
+      
+//       console.log(`Add new contact ${contactId} completed`);
+//     })
+//     .then(() => {
+//       contacts.deleteContact(contactId);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+
+// };
+
+//using async/await
+const addNewContact = async (user, newContact) => {
+    const { userId } = user;
+    const { preferredName, familyName, phoneNumber, email } = newContact;
+
+    try {
+      if (await contacts.verifyContactExists(user, newContact)) {
+        throw `Contact '${preferredName} ${familyName}' already exists for user ${userId}`;
       }
 
-      return contacts.addContact(user, newContact);
-    })
-    .then((data) => {
-      contactId = data.insertId;
-      console.log(`Contact '${preferredName} ${familyName}' added for userId = ${userId}, contactId = ${contactId}`);
+      const contactId = await contacts.addContact(user, newContact);
+      console.log(`Contact '${preferredName} ${familyName}' added for userId: ${userId}, contactId: ${contactId}`);
 
-      return phoneNumbers.addPhoneNumber(contactId, phoneNumber);
-    })
-    .then((data) => {
-      phoneNumberId = data.insertId;
-      console.log(`Phone number '${phoneNumber}' added for contactId = ${contactId}, phoneNumberId = ${phoneNumberId}`);
-
-      return emails.addEmail(contactId, email);
-    })
-    .then((data) => {
-      emailId = data.insertId;
-      console.log(`Email '${email}' added for contactId = ${contactId}, emailId = ${emailId}`);
-      
+      const phoneNumberId = await phoneNumbers.addPhoneNumber(contactId, phoneNumber);
+      console.log(`Phone number '${phoneNumber}' added for contactId: ${contactId}, phoneNumberId: ${phoneNumberId}`);
+    
+      const emailId = await emails.addEmail(contactId, email);
+      console.log(`Email '${email}' added for contactId: ${contactId}, emailId: ${emailId}`);
+          
       console.log(`Add new contact ${contactId} completed`);
-    })
-    .then(() => {
       contacts.deleteContact(contactId);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 
-};
+    } catch(err) {
+      console.log(err);
+    }
+  };
 
 addNewContact(user, contact1);
