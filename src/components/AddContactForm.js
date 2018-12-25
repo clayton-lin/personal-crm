89b256d331acb3cf33d590ec5b2631dce2859426
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-24 21:22:34 -1000

    Init AddContactForm component

---
+++ src/components/AddContactForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AddContactForm extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default AddContactForm;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import AddContactForm from './AddContactForm';
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Personal-CRM App</h1>
+        <AddContactForm/>
       </div>
     );
   }
