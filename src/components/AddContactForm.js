4750ee7639ee7b8fb4b0eff4c66f14c9b6abe495
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-24 21:22:58 -1000

    Add constructor and state

--- src/components/AddContactForm.js
+++ src/components/AddContactForm.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 class AddContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newContactInfo: {
+          userId: null,
+          contactId: null,
+          givenName: '',
+          preferredName: '',
+          middleName: '',
+          familyName: '', 
+          maidenName: '',
+          gender: '',
+          birthYear: null,
+          birthMonth: null,
+          birthDay: null,
+          phoneNumber: '',
+          email: ''
+      }
+    };
+
+
+
+
+
+
+  }
+
 
   render() {
     return (
       <div>
-        
+        <h1>AddContactForm</h1>
       </div>
     );
   }
